901b7524ece545fb4ddc4be876747c2db31002b7
Karim Anaya-Izquierdo <kai21@vpx2l6g20h.campus.bath.ac.uk>
2025-11-21 10:25:56 +0000

    Fix: Ensure ThemeProvider and SearchProvider are correctly wrapped in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
-import { HeadProvider } from 'react-head'; // Import HeadProvider
+import { HeadProvider } from 'react-head';
 import { TabProvider } from './context/TabContext';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { ThemeProvider } from './context/ThemeContext'; // Import ThemeProvider
+import { SearchProvider } from './context/SearchContext'; // Import SearchProvider
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap with BrowserRouter */}
+    <BrowserRouter>
       <HeadProvider>
-        <TabProvider>
-          <App />
-        </TabProvider>
+        <ThemeProvider>
+          <SearchProvider>
+            <TabProvider>
+              <App />
+            </TabProvider>
+          </SearchProvider>
+        </ThemeProvider>
       </HeadProvider>
     </BrowserRouter>
   </React.StrictMode>,
